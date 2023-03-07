5311897d7dafcf4ecdabcb04a75c360d2c40c79a
Carolina Urrutia Iturrieta <carolina.urrutia.iturrieta@gmail.com>
2023-03-07 02:04:14 +0000

    flux ok

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,19 +8,14 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Bienvenidos</h1>
+			<div className="alert alert-info"></div>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src="https://img.freepik.com/vector-gratis/ilustracion-colorida-programador-femenino-trabajando_23-2148277397.jpg"/>
 			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
